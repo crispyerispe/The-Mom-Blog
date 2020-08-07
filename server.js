a88894d56832a601b50466201150160e8814ae4a
crispyerispe <chelseaerispe3@gmail.com>
2020-08-07 14:30:37 +0200

    Changes

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
-});
-
 app.get("/", (req, res) => res.json({ msg: "Welcome" }));
 
 const PORT = process.env.PORT || 8000;
